97872caf9ad9c68e3f388c354afef4687e7c7c88
VigneshKumar <jarvis@bravetux.com>
2018-05-23 01:16:02 +0530

    Removing unnecessary edits

--- pelicanconf.py
+++ pelicanconf.py
@@ -32,8 +32,9 @@ SOCIAL = (('You can add links in your config file', '#'),
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-# RELATIVE_URLS = True
-RELATIVE_URLS = False
+RELATIVE_URLS = True
+
+# Pagination Enabled
 PAGINATED_DIRECT_TEMPLATES = ('categories', 'archives')
 
 #Backdrop Theme
